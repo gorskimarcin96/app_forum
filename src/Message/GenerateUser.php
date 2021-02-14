cfa62d0a6a7072feebcb5b499aa31d1ae93f0b73
Marcin Górski <gorskimarcin96@gmail.com>
2021-02-14 21:32:15 +0100

    add messages (symfony queue), generateUserJob and icons to footer

---
+++ src/Message/GenerateUser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Message;
+
+final class GenerateUser
+{
+    /**
+     * @var int
+     */
+    private $limit;
+
+    /**
+     * GenerateUser constructor.
+     * @param int $limit
+     */
+    public function __construct($limit = 1000)
+    {
+        $this->limit = $limit;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLimit(): int
+    {
+        return $this->limit;
+    }
+}
---
+++ src/MessageHandler/GenerateUserHandler.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\MessageHandler;
+
+use App\Document\User;
+use App\Message\GenerateUser;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\MongoDBException;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+final class GenerateUserHandler implements MessageHandlerInterface
+{
+    /**
+     * @var DocumentManager
+     */
+    private $dm;
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $pe;
+
+    /**
+     * GenerateUser constructor.
+     * @param DocumentManager $documentManager
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function __construct(DocumentManager $documentManager, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->dm = $documentManager;
+        $this->pe = $passwordEncoder;
+    }
+
+    /**
+     * @param GenerateUser $message
+     * @throws MongoDBException
+     */
+    public function __invoke(GenerateUser $message)
+    {
+        foreach (range(0, $message->getLimit()) as $item) {
+            $user[$item] = new User();
+            $user[$item]->setPassword($this->pe->encodePassword($user[$item], 'password'));
+            $user[$item]->setIsVerified(true);
+            $this->dm->persist($user[$item]);
+            $user[$item]->setEmail($user[$item]->getId() . '@test.pl');
+            $this->dm->persist($user[$item]);
+        }
+        $this->dm->flush();
+    }
+}
